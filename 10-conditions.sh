f7b225d097368cefd08a187e96d50efe86f03568
keerthi <reddychitti15@gmail,com>
2025-09-23 13:12:23 +0530

    conditions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,8 +2,10 @@
 
 NUMBER=$1
 
-if [$NUMBER -1T 10]; then
+if [$NUMBER -lt 10]; then
     echo "Given number $NUMBER is less then 10"
+    else if [$NUMBER -wq 10]; then
+    echo "Given number $NUMBER is equal to 10"
 else
     echo "Given number $NUMBER is greter then or equal 10"
 fi
