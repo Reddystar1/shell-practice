9db898da8db6b81a11bf5a2fb89f1b1e2309702a
keerthi <reddychitti15@gmail,com>
2025-09-23 14:22:13 +0530

    ERROR-SUCCESS

---
+++ 13-installation.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0]; then
+    echo "ERROR:: please run this script with root prevlege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: installing mysql is failure"
+else
+    echo "Installing mysql is SUCCESS" 
