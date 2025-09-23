adb64adc30f4af99cb67feccd7938ea82a33a40d
keerthi <reddychitti15@gmail,com>
2025-09-23 14:28:07 +0530

    ERROR-SUCCESS

--- 13-installation.sh
+++ 13-installation.sh
@@ -4,12 +4,15 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: please run this script with root prevlege"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: installing mysql is failure"
+    exit 1
 else
     echo "Installing mysql is SUCCESS" 
+    exit 0
 fi
